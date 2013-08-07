df435fdb24e18855449f7b8aa5da792b5c7a3ae6
Marcin Warpechowski <warpech@gmail.com>
2013-08-07 13:01:34 +0200

    fix view models to be used with MailAddress class

--- MailPage.json.cs
+++ MailPage.json.cs
@@ -4,6 +4,14 @@ partial class MailPage : Json<Mail> {
     public string Uri {
     get {
         return "/mails/" + Data.Id;
+    }
    }
+
+  [MailPage.json.From]
+  partial class FromObj : Json<MailAddress> {
+  }
+
+  [MailPage.json.To]
+  partial class ToObj : Json<MailAddress> {
   }
 }
