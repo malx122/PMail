da39e69cfdf94e58a546671fb3fb056eb4fbda1b
Marcin Warpechowski <warpech@gmail.com>
2013-08-12 17:23:55 +0200

    connect MailAddress with <datalist> (#10, #13)

--- MailPage.json.cs
+++ MailPage.json.cs
@@ -15,6 +15,14 @@ partial class MailPage : Json<Mail> {
   partial class ToObj : Json<MailAddress> {
   }
 
+  [MailPage.json.To.Options]
+  partial class ToOptionsObj : Json<MailAddress> {
+  }
+
+  void Handle(Input.To.Address input) {
+      this.To.Options = Db.SQL("SELECT a FROM MailAddress a WHERE a.Address STARTS WITH ?", input.Value);
+  }
+
   void Handle(Input.Send input) {
       this.Transaction.Commit();
   }
