58c09124e3b92df8149295d8ce011651b24f7849
Marcin Warpechowski <warpech@gmail.com>
2013-08-15 12:05:46 +0200

    contact editing form (issue #22)

--- Contact.cs
+++ Contact.cs
@@ -6,9 +6,29 @@ public class Contact {
     public string FirstName;
     public string LastName;
 
+    public string DisplayName {
+        get {
+            if (FirstName != "" && LastName != "") {
+                return LastName + ", " + FirstName;
+            }
+            else if (FirstName != "") {
+                return FirstName;
+            }
+            else {
+                return LastName;
+            }
+        }
+    }
+
     public string Uri {
         get {
             return "/contacts/" + Id;
+        }
+    }
+
+    public SqlResult<MailAddress> Addresses {
+        get {
+            return Db.SQL<MailAddress>("SELECT m FROM MailAddress m WHERE Contact=?", this);
         }
     }
 }
--- ContactPage.json.cs
+++ ContactPage.json.cs
@@ -2,5 +2,28 @@ using Starcounter;
 using System;
 
 partial class ContactPage : Json<Contact> {
+    [ContactPage.json._Addresses]
+    partial class AddressesObj : Json<MailAddress> {
+        void Handle(Input._Addresses.Address input) {
+            this.Address = input.Value;
+            this.Transaction.Commit();
+        }
+    }
 
+    void Handle(Input.FirstName input) {
+        this.FirstName = input.Value;
+        this.Transaction.Commit();
+    }
+
+    void Handle(Input.LastName input) {
+        this.LastName = input.Value;
+        this.Transaction.Commit();
+    }
+
+    void Handle(Input.AddAddress input) {
+        var address = new MailAddress();
+        address.Contact = this.Data;
+        this._Addresses.Add().Data = address;
+        this.Transaction.Commit();
+    }
 }
