62acca376f85fdd4fd771944c3486b8e81e26adf
Marcin Warpechowski <warpech@gmail.com>
2013-09-03 22:28:14 +0200

    PuppetJs current version (shows 4xx and 5xx errors and implements $parent object reference)

--- Client/vendor/puppet.js
+++ Client/vendor/puppet.js
@@ -10,6 +10,7 @@
    * @param callback Called after initial state object is received from the server
    */
   function Puppet(remoteUrl, callback) {
+    this.debug = true;
     this.remoteUrl = remoteUrl;
     this.callback = callback;
     this.obj = null;
@@ -18,38 +19,6 @@
     this.queue = [];
     this.handleResponseCookie();
     this.xhr(this.remoteUrl, 'application/json', null, this.bootstrap.bind(this));
-  }
-
-  //http://www.quirksmode.org/js/cookies.html
-  function createCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
-    }
-    document.cookie = name + "=" + value + expires + '; path=/';
-  }
-
-  function readCookie(name) {
-    return readCookies()[name];
-  }
-
-  //http://www.quirksmode.org/js/cookies.html
-  function eraseCookie(name) {
-    createCookie(name, "", -1);
-  }
-
-  //https://github.com/js-coder/cookie.js/blob/gh-pages/cookie.js
-  function readCookies() {
-    if (document.cookie === '') return {};
-    var cookies = document.cookie.split('; ')
-      , result = {};
-    for (var i = 0, l = cookies.length; i < l; i++) {
-      var item = cookies[i].split('=');
-      result[decodeURIComponent(item[0])] = decodeURIComponent(item[1]);
-    }
-    return result;
   }
 
   /**
@@ -64,14 +33,42 @@
    */
   var PuppetJsClickTrigger$ = "\u2400";
 
-  function recursiveMarkObjNulls(obj) {
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (typeof key === "string" && obj[key] === null) {
-          obj[key] = PuppetJsClickTrigger$;
+  function markObjPropertyByPath(obj, path) {
+    var keys = path.split('/');
+    var len = keys.length;
+    if (keys.length > 2) {
+      for (var i = 1; i < len - 1; i++) {
+        obj = obj[keys[i]];
+      }
+    }
+    recursiveMarkObjProperties(obj, keys[len - 1]);
+  }
+
+  function placeMarkers(parent, key) {
+    var subject = parent[key];
+    if (subject === null) {
+      parent[key] = PuppetJsClickTrigger$;
+    }
+    else if (typeof subject === 'object' && !subject.hasOwnProperty('$parent')) {
+      Object.defineProperty(subject, '$parent', {
+        enumerable: false,
+        get: function () {
+          return parent;
         }
-        else if (typeof obj[key] === "object") {
-          recursiveMarkObjNulls(obj[key]);
+      });
+    }
+  }
+
+  function recursiveMarkObjProperties(parent, key) {
+    if (key && parent) {
+      placeMarkers(parent, key);
+      parent = parent[key];
+    }
+
+    if (parent) {
+      for (var i in parent) {
+        if (parent.hasOwnProperty(i) && typeof parent[i] === 'object') {
+          recursiveMarkObjProperties(parent, i);
         }
       }
     }
@@ -79,7 +76,7 @@
 
   Puppet.prototype.bootstrap = function (event) {
     this.obj = JSON.parse(event.target.responseText);
-    recursiveMarkObjNulls(this.obj);
+    recursiveMarkObjProperties(this.obj);
     this.observe();
     if (this.callback) {
       this.callback(this.obj);
@@ -107,10 +104,10 @@
    * The cookie will be erased (replaced with empty value) after reading
    */
   Puppet.prototype.handleResponseCookie = function () {
-    var location = readCookie('Location');
+    var location = cookie.read('Location');
     if (location) { //if cookie exists and is not empty
       this.referer = location;
-      eraseCookie('Location');
+      cookie.erase('Location');
     }
   };
 
@@ -163,14 +160,15 @@
     }
     this.xhr(this.referer || this.remoteUrl, 'application/json-patch+json', txt, this.handleRemoteChange.bind(this));
     var that = this;
+    this.unobserve();
     patches.forEach(function (patch) {
       if ((patch.op === "add" || patch.op === "replace" || patch.op === "test") && patch.value === null) {
-        that.unobserve();
         patch.value = PuppetJsClickTrigger$;
         jsonpatch.apply(that.obj, [patch]);
-        that.observe();
       }
+      markObjPropertyByPath(that.obj, patch.path);
     });
+    this.observe();
   };
 
   Puppet.prototype.handleRemoteChange = function (event) {
@@ -181,13 +179,14 @@
     if (patches.length === void 0) {
       throw new Error("Patches should be an array");
     }
-    patches.forEach(function (patch) {
-      if (patch.op === "add" || patch.op === "replace" || patch.op === "test") {
-        recursiveMarkObjNulls(patch);
-      }
-    });
     this.unobserve();
     jsonpatch.apply(this.obj, patches);
+    var that = this;
+    patches.forEach(function (patch) {
+      if (patch.op === "add" || patch.op === "replace" || patch.op === "test") {
+        markObjPropertyByPath(that.obj, patch.path);
+      }
+    });
     this.observe();
     if (this.onRemoteChange) {
       this.onRemoteChange(patches);
@@ -222,27 +221,47 @@
     this.changeState(location.href);
   };
 
+  Puppet.prototype.showError = function (heading, description) {
+    if (this.debug) {
+      var DIV = document.getElementById('puppetjs-error');
+      if(!DIV) {
+        DIV = document.createElement('DIV');
+        DIV.id = 'puppetjs-error';
+        DIV.style.border = '1px solid #dFb5b4';
+        DIV.style.background = '#fcf2f2';
+        DIV.style.padding = '10px 16px';
+        if (document.body.firstChild) {
+          document.body.insertBefore(DIV, document.body.firstChild);
+        }
+        else {
+          document.body.appendChild(DIV);
+        }
+      }
+
+      var H1 = document.createElement('H1');
+      H1.innerHTML = heading;
+
+      var PRE = document.createElement('PRE');
+      PRE.innerHTML = description;
+      PRE.style.whiteSpace = 'pre-wrap';
+
+      DIV.appendChild(H1);
+      DIV.appendChild(PRE);
+    }
+    throw new Error(description);
+  };
+
   Puppet.prototype.xhr = function (url, accept, data, callback) {
     //this.handleResponseCookie();
-    eraseCookie('Location'); //more invasive cookie erasing because sometimes the cookie was still visible in the requests
+    cookie.erase('Location'); //more invasive cookie erasing because sometimes the cookie was still visible in the requests
 
     var req = new XMLHttpRequest();
     var that = this;
     req.addEventListener('load', function (event) {
       that.handleResponseCookie();
       that.handleResponseHeader(event.target);
-      if (event.target.status >= 400 && event.target.status < 599) {
-        
-         var str = "Server responded with error " + event.target.status + " (" + event.target.statusText + ")";
-
-        if (event.target.status >= 500) {
-           var doc = document.open("text/html");
-           str += "\n" + event.target.response;
-           doc.write(str.replace(/\n/g, "<br />"));
-           doc.close();
-           return;
-        }
-        throw new Error( str );
+      if (event.target.status >= 400 && event.target.status <= 599) {
+        that.showError('PuppetJs JSON response error', 'Server responded with error ' + event.target.status + ' ' + event.target.statusText + '\n\n' + event.target.responseText);
       }
       else {
         callback.call(that, event);
@@ -269,6 +288,42 @@
     element.addEventListener("click", function (event) {
       window.PuppetExternalLink = event.target;
     });
+  };
+
+  /**
+   * Cookie helper
+   * reference: http://www.quirksmode.org/js/cookies.html
+   * reference: https://github.com/js-coder/cookie.js/blob/gh-pages/cookie.js
+   */
+  var cookie = {
+    create: function createCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+      document.cookie = name + "=" + value + expires + '; path=/';
+    },
+
+    readAll: function readCookies() {
+      if (document.cookie === '') return {};
+      var cookies = document.cookie.split('; ')
+        , result = {};
+      for (var i = 0, l = cookies.length; i < l; i++) {
+        var item = cookies[i].split('=');
+        result[decodeURIComponent(item[0])] = decodeURIComponent(item[1]);
+      }
+      return result;
+    },
+
+    read: function readCookie(name) {
+      return cookie.readAll()[name];
+    },
+
+    erase: function eraseCookie(name) {
+      cookie.create(name, "", -1);
+    }
   };
 
   global.Puppet = Puppet;
