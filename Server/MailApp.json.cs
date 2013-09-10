3f5821dd8dfa84eadeb4675f5af662471e2acc83
Marcin Warpechowski <warpech@gmail.com>
2013-09-10 10:18:12 +0200

    highlight link to the current application

--- Server/MailApp.json.cs
+++ Server/MailApp.json.cs
@@ -16,6 +16,7 @@ partial class MailApp : Page {
     /// JSON objects can be manipulated on the server at any time. In this method, we add some items to a JSON array
     /// containing menu choices. These will be rendered on the client in the main side bar menu.
     public void AddSomeNiceMenuItems(Master master) {
+        master.ApplicationName = "Mails";
         master.Menu.Items.Clear();                  // These are just JSON properties we created in the Master.json file. They can have any name and value.
         var create = master.Menu.Items.Add();       // Adds a new item to an array. The { Menu: Items: [ { ... } ] } can be found in Master.json.
         create.Label = "Compose new email";         // Let's have a menu button to allow us to create new emails.
